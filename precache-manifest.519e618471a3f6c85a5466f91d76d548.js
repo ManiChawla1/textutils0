3ea30ec0e9a0105a2362a07fbcad0602500cc7dd
ManiChawla1 <96613508+ManiChawla1@users.noreply.github.com>
2022-04-09 17:26:48 +0530

    Updates

---
+++ precache-manifest.519e618471a3f6c85a5466f91d76d548.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae11c03c819484137f87",
+    "url": "/textutils0/static/css/main.8654e551.chunk.css"
+  },
+  {
+    "revision": "ae11c03c819484137f87",
+    "url": "/textutils0/static/js/main.206444ac.chunk.js"
+  },
+  {
+    "revision": "9a62176942b8e768a633",
+    "url": "/textutils0/static/js/runtime~main.0495300c.js"
+  },
+  {
+    "revision": "f7cca44883c73aeaefd8",
+    "url": "/textutils0/static/js/2.22d00ad1.chunk.js"
+  },
+  {
+    "revision": "68c47fc33538f58652f4",
+    "url": "/textutils0/static/js/3.987c3a98.chunk.js"
+  },
+  {
+    "revision": "82c956e04675353e32e491ba965bcdee",
+    "url": "/textutils0/index.html"
+  }
+];
